8ebb6226c8c995ea447c91fa5fee8e163810e6e1
dakita <daisuke.akita.zj@renesas.com>
2024-07-19 15:13:25 +0900

    Add GitHub Actions workflow for Docker build and push

---
+++ python-helloworld/app.py
@@ -0,0 +1,40 @@
+import logging
+from flask import Flask
+from flask import json
+
+app = Flask(__name__)
+
+@app.route('/status')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    ## log line
+    app.logger.info('Status request successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    ## log line
+    app.logger.info('Metrics request successfull')
+    return response
+
+@app.route("/")
+def hello():
+    # Logging a CUSTOM message 
+    app.logger.info('Main request successfull')
+    return "Hello World!"
+
+if __name__ == "__main__":
+     # Stream logs to a file, and set the default log level to DEBUG
+     logging.basicConfig(filename='app.log',level=logging.DEBUG)
+     app.run(host='0.0.0.0', port=5001)
